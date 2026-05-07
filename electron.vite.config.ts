d62268b9d914791f1d335a0f121ebc736b54e329
Ziang Xie <xieziang0814@gmail.com>
2026-05-07 14:04:34 +0800

    Bundle runtime dependencies for mac packaging

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,10 +1,9 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
-import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import { defineConfig } from "electron-vite";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
         input: {
